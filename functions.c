53d6f5307a3041a07e65b40025cca3cd7e1888ac
EduardoZimermam <eduardozimerman@live.com>
2018-09-04 23:37:40 -0300

    Implementação das primeiras funções

--- functions.c
+++ functions.c
@@ -2,3 +2,21 @@
 Annelyse Schatzmann           GRR20151731
 Eduardo Zimermam Pereira      GRR20152952  */
 
+#include "functions.h"
+
+
+double** alocaMatriz(int linhas, int colunas){
+	double **matriz;
+
+	matriz = (double**) malloc((linhas*colunas) * sizeof(double));
+
+	for(int i = 0; i < linhas; i++){
+		matriz[i] = matriz[i * linhas];
+	}
+
+	return matriz;
+}
+
+double gradienteConjugado(double **matriz, double *b, int maxIt, double eps){
+
+}
