fb5c829b89a7fbc21b2a7cba6f09918f4bcf9259
EduardoZimermam <eduardozimerman@live.com>
2018-09-13 23:43:36 -0300

    Criado o pré-condicionador de Jacobi

--- functions.c
+++ functions.c
@@ -28,9 +28,16 @@ double** alocaMatriz(int linhas, int colunas){
 }
 
 void printMatriz(double** matriz, int linhas, int colunas){
-	for (int i = 0; i < linhas; i*colunas){
-		for(int j = 0; j < i * colunas; j++){
-			printf("%.15g ", matriz[i][j]);
+	// for (int i = 0; i < linhas; i*colunas){
+	// 	for(int j = 0; j < i * colunas; j++){
+	// 		printf("%.15g ", matriz[i][j]);
+	// 	}
+	// 	printf("\n");
+	// }
+
+	for (int i = 0; i < linhas; i++) {
+		for (int j = 0; j < colunas; j++) {
+			printf("%lf ", matriz[i][j]);
 		}
 		printf("\n");
 	}
@@ -92,82 +99,102 @@ double* multiplica_matriz_vetor(double **matriz, double *vetorA, int tamVetor, d
   }
 }
 
-double* gradienteConjugado(double **matriz, double *vetor, double MaxIt, double eps, int tamVetor){
-	double *X, *z, *r, *v, *vet_aux, escalar, aux1;
+// double* gradienteConjugado(double **matriz, double *vetor, double MaxIt, double eps, int tamVetor){
+// 	double *X, *z, *r, *v, *vet_aux, escalar, aux1;
+//
+//   X = alocaVetor(tamVetor);
+// 	z = alocaVetor(tamVetor);
+// 	vet_aux = alocaVetor(tamVetor);
+//
+// 	r = copiaVetor(vetor, tamVetor);
+// 	v = copiaVetor(vetor, tamVetor);
+//
+// 	double aux = produtoInterno_vetor(r, r, tamVetor);
+//
+//
+// 	for(int itr = 0; itr < MaxIt; itr++){
+// 		multiplica_matriz_vetor(matriz, v, tamVetor, z);
+// 		escalar = produtoInterno_vetor(v, z, tamVetor);
+// 		double s = aux / escalar;
+//
+// 		multiplica_escalarVetor(v, s, tamVetor, vet_aux);
+// 		soma_vetor(X, vet_aux, tamVetor, X);
+//
+// 		multiplica_escalarVetor(z, s, tamVetor, vet_aux);
+// 		subtrai_vetor(r, vet_aux, tamVetor, r);
+// 		aux1 = produtoInterno_vetor(r, r, tamVetor);
+//
+// 		if(aux1 < eps){
+// 			return(X);
+// 		}
+//
+// 		double m = aux1/aux;
+// 		aux = aux1;
+//
+// 		multiplica_escalarVetor(v, m, tamVetor, vet_aux);
+// 		soma_vetor(r, vet_aux, tamVetor, v);
+// 	}
+// }
 
-  X = alocaVetor(tamVetor);
-	z = alocaVetor(tamVetor);
-	vet_aux = alocaVetor(tamVetor);
+double** preCond_Jacobi(double** matriz, int linhas, int colunas){
+	double **matrizPreCond = (double**) malloc(linhas * sizeof(double**));
 
-	r = copiaVetor(vetor, tamVetor);
-	v = copiaVetor(vetor, tamVetor);
-	
-	double aux = produtoInterno_vetor(r, r, tamVetor);
-	
-
-	for(int itr = 0; itr < MaxIt; itr++){
-		multiplica_matriz_vetor(matriz, v, tamVetor, z);
-		escalar = produtoInterno_vetor(v, z, tamVetor);
-		double s = aux / escalar;
-
-		multiplica_escalarVetor(v, s, tamVetor, vet_aux);
-		soma_vetor(X, vet_aux, tamVetor, X);
-
-		multiplica_escalarVetor(z, s, tamVetor, vet_aux);
-		subtrai_vetor(r, vet_aux, tamVetor, r);
-		aux1 = produtoInterno_vetor(r, r, tamVetor);
-
-		if(aux1 < eps){
-			return(X);	
-		}
-
-		double m = aux1/aux;
-		aux = aux1;
-
-		multiplica_escalarVetor(v, m, tamVetor, vet_aux);
-		soma_vetor(r, vet_aux, tamVetor, v);
+	for (int i = 0; i < linhas; i++) {
+		matrizPreCond[i] = (double*) malloc(colunas * sizeof(double*));
 	}
+
+
+	for (int i = 0; i < linhas; i++) {
+		for (int j = 0; j < colunas; j++) {
+			matrizPreCond[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < linhas; i++) {
+		matrizPreCond[i][i] = matriz[i][i];
+	}
+
+	return (matrizPreCond);
 }
 
-
-double* gradConj_comPreCondicionador(double **matriz, double *vetor, double **M, double MaxIt, double eps, int tamVetor){
-	double *X, *y, *z, *r, *v, *vet_aux, escalar, aux1;
-
-  X = alocaVetor(tamVetor);
-  v = alocaVetor(tamVetor);
-  y = alocaVetor(tamVetor);
-	z = alocaVetor(tamVetor);
-	vet_aux = alocaVetor(tamVetor);
-
-	r = copiaVetor(vetor, tamVetor);
-	multiplica_matriz_vetor(M, vetor, tamVetor, v);  /* v = (M*b) */
-	multiplica_matriz_vetor(M, r, tamVetor, y);			/* y = (M*r) */
-	
-	double aux = produtoInterno_vetor(y, r, tamVetor);
-	
-	for(int itr = 0; itr < MaxIt; itr++){
-		multiplica_matriz_vetor(matriz, v, tamVetor, z);
-		escalar = produtoInterno_vetor(v, z, tamVetor);
-		double s = aux / escalar;
-
-		multiplica_escalarVetor(v, s, tamVetor, vet_aux);
-		soma_vetor(X, vet_aux, tamVetor, X);
-
-		multiplica_escalarVetor(z, s, tamVetor, vet_aux);
-		subtrai_vetor(r, vet_aux, tamVetor, r);
-
-		multiplica_matriz_vetor(M, r, tamVetor, y);			/* y = (M*r) */
-			
-		if((produtoInterno_vetor(r, r, tamVetor)) < eps){
-			return(X);	
-		}
-
-		aux1 = produtoInterno_vetor(y, r, tamVetor);
-
-		double m = aux1 / aux;
-		aux = aux1;
-
-		multiplica_escalarVetor(v, m, tamVetor, vet_aux);
-		soma_vetor(y, vet_aux, tamVetor, v);
-	}
-}
+// double* gradConj_comPreCondicionador(double **matriz, double *vetor, double **M, double MaxIt, double eps, int tamVetor){
+// 	double *X, *y, *z, *r, *v, *vet_aux, escalar, aux1;
+//
+//   X = alocaVetor(tamVetor);
+//   v = alocaVetor(tamVetor);
+//   y = alocaVetor(tamVetor);
+// 	z = alocaVetor(tamVetor);
+// 	vet_aux = alocaVetor(tamVetor);
+//
+// 	r = copiaVetor(vetor, tamVetor);
+// 	multiplica_matriz_vetor(M, vetor, tamVetor, v);  /* v = (M*b) */
+// 	multiplica_matriz_vetor(M, r, tamVetor, y);			/* y = (M*r) */
+//
+// 	double aux = produtoInterno_vetor(y, r, tamVetor);
+//
+// 	for(int itr = 0; itr < MaxIt; itr++){
+// 		multiplica_matriz_vetor(matriz, v, tamVetor, z);
+// 		escalar = produtoInterno_vetor(v, z, tamVetor);
+// 		double s = aux / escalar;
+//
+// 		multiplica_escalarVetor(v, s, tamVetor, vet_aux);
+// 		soma_vetor(X, vet_aux, tamVetor, X);
+//
+// 		multiplica_escalarVetor(z, s, tamVetor, vet_aux);
+// 		subtrai_vetor(r, vet_aux, tamVetor, r);
+//
+// 		multiplica_matriz_vetor(M, r, tamVetor, y);			/* y = (M*r) */
+//
+// 		if((produtoInterno_vetor(r, r, tamVetor)) < eps){
+// 			return(X);
+// 		}
+//
+// 		aux1 = produtoInterno_vetor(y, r, tamVetor);
+//
+// 		double m = aux1 / aux;
+// 		aux = aux1;
+//
+// 		multiplica_escalarVetor(v, m, tamVetor, vet_aux);
+// 		soma_vetor(y, vet_aux, tamVetor, v);
+// 	}
+// }
--- functions.h
+++ functions.h
@@ -19,6 +19,7 @@ double* soma_vetor(double*, double*, int, double*);
 double* subtrai_vetor(double*, double*, int, double*);
 double* multiplica_matriz_vetor(double**, double*, int, double*);
 
+double** preCond_Jacobi(double**, int, int);
 double gradienteConjugado(double**, double*, double*, double, int);
 double gradConj_comPreCondicionador(double**, double*, double*, double);
 
