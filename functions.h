1727ea5cfd5920158faa34b24d61d5dad1878d10
EduardoZimermam <eduardozimerman@live.com>
2018-09-16 21:30:51 -0300

    Alterado o comentário.

--- functions.h
+++ functions.h
@@ -2,10 +2,10 @@
  *  @author Annelyse Schatzmann           GRR20151731
  *  @author Eduardo Zimermam Pereira      GRR20152952
  */
-  
+
 /**
  * @file functions.h
- * @brief Todas os métodos utilizados na execução do programa.
+ * @brief Todas as funções utilizadas nos métodos.
  */
 
 #ifndef __FUNCTIONS__
